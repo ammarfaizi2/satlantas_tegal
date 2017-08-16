6018ddda909a1b10a62e6b52f3fc5fb18353a2e9
Ammar Faizi <ammarfaizi2@gmail.com>
2017-08-17 01:10:57 +0700

    Update config for mobile development

--- bot/Models/config.php
+++ bot/Models/config.php
@@ -4,13 +4,13 @@ define("DB_USER", "tegaljat_lantas");
 define("DB_PASS", "triosemut123");
 define("DB_NAME", "tegaljat_lantas");*/
 
-define("DB_HOST", "localhost");
+/*define("DB_HOST", "localhost");
 define("DB_USER", "debian-sys-maint");
 define("DB_PASS", "");
-define("DB_NAME", "tegal");
+define("DB_NAME", "tegal");*/
 
-/*define("DB_HOST", "localhost");
+define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "");
 define("DB_NAME", "tegal");
-*/
+
