c8bab591ad443bc1b0b5bf31229ad393c53cf6e3
Ammar Faizi <ammarfaizi2@gmail.com>
2017-08-24 00:04:59 +0700

    Update config

--- bot/Models/config.php
+++ bot/Models/config.php
@@ -1,14 +1,14 @@
 <?php
-/*define("DB_HOST", "localhost");
+define("DB_HOST", "localhost");
 define("DB_USER", "tegaljat_lantas");
 define("DB_PASS", "triosemut123");
-define("DB_NAME", "tegaljat_lantas");*/
+define("DB_NAME", "tegaljat_lantas");
 
-define("DB_HOST", "localhost");
+/*define("DB_HOST", "localhost");
 define("DB_USER", "debian-sys-maint");
 define("DB_PASS", "");
 define("DB_NAME", "tegal");
-
+*/
 /*define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "");
